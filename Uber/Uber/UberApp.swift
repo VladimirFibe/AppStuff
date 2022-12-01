e7b69fa50057d512b47beb23c69891a2d46befca
Vladimir Fibe <motiw@icloud.com>
2022-12-01 06:26:26 +0600

    Uber

---
+++ Uber/Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Vladimir Fibe on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
