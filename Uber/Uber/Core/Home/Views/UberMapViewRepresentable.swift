4eda461afccf7f3927b0d25f06762a00541792f7
Vladimir Fibe <motiw@icloud.com>
2022-12-05 21:32:01 +0600

    02.11.31

--- Uber/Uber/Core/Home/Views/UberMapViewRepresentable.swift
+++ Uber/Uber/Core/Home/Views/UberMapViewRepresentable.swift
@@ -18,6 +18,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
             context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
         }
     }
     
@@ -30,7 +31,7 @@ extension UberMapViewRepresentable {
     class MapCoordinator: NSObject, MKMapViewDelegate {
         //MARK: - Properties
         let parent: UberMapViewRepresentable
-        
+        var userLocationCoordinate: CLLocationCoordinate2D?
         //MARK: - Lifecycle
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
@@ -39,6 +40,7 @@ extension UberMapViewRepresentable {
         
         //MARK: - MKMapViewDelegate
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(
                 center: userLocation.coordinate,
                 span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
@@ -54,6 +56,40 @@ extension UberMapViewRepresentable {
             parent.mapView.addAnnotation(anno)
             parent.mapView.selectAnnotation(anno, animated: true)
             parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                 to destation: CLLocationCoordinate2D,
+                                 completion: @escaping (MKRoute) -> Void) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destinationPlacemark = MKPlacemark(coordinate: destation)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destinationPlacemark)
+            let directions = MKDirections(request: request)
+            directions.calculate { response, error in
+                if let error = error {
+                    print("DEBUG: \(error.localizedDescription)")
+                    return
+                }
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
+        }
+        
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            getDestinationRoute(from: userLocationCoordinate,
+                                to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let way = MKPolylineRenderer(overlay: overlay)
+            way.strokeColor = .systemBlue
+            way.lineWidth = 6
+            return way
         }
     }
 }
